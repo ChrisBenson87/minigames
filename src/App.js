e74cce66676696f4ceaddc8ef3711d8d75ce67f9
Chris Benson <ChrisBenson87@gmail.com>
2022-07-16 23:14:46 +0100

    trying stuff

--- src/App.js
+++ src/App.js
@@ -4,29 +4,36 @@ import {Level2} from "./components/level2";
 import {Level3} from "./components/level3";
 import {Level4} from "./components/level4";
 import {Level5} from "./components/level5";
-import {useState} from "react";
 
 function App() {
-
-  let [levelSelection, setLevelSelection] = useState([Level1, Level2, Level3, Level4, Level5]);
 
   let currentLevel = 0;
 
   const levelSelect = () => {
     currentLevel = Math.floor(Math.random() * 5 + 1);
-    setLevelSelection = currentLevel;
+    console.log(currentLevel)
   }
+
+  if(currentLevel = 1) {
+    return <Level1 />}
+
+  else if(currentLevel = 2) {
+    return <Level2 />}
+  
+  else if(currentLevel = 3) {
+    return <Level3 />}
+  
+  else if(currentLevel = 4) {
+    return <Level4 />}
+  
+  else if(currentLevel = 5) {
+    return <Level5 />}
 
   return (
     <div className="App">
       <img></img>
       <div className = "startGame">
         <button onClick = {levelSelect}>Start Game</button>
-        <Level1 levelSelection = {levelSelection} />
-        <Level2 levelSelection = {levelSelection} />
-        <Level3 levelSelection = {levelSelection} />
-        <Level4 levelSelection = {levelSelection} />
-        <Level5 levelSelection = {levelSelection} />
       </div>
     </div>
   );
