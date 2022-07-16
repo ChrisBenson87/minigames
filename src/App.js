45ee6d914fcf95bbd3dbd8cdcb46ef08a61080e8
Chris Benson <ChrisBenson87@gmail.com>
2022-07-16 23:00:34 +0100

    levelselector

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import './App.css';
-import {level1} from "./components/level1";
-import {level2} from "./components/level2";
-import {level3} from "./components/level3";
-import {level4} from "./components/level4";
-import {level5} from "./components/level5";
+import {Level1} from "./components/level1";
+import {Level2} from "./components/level2";
+import {Level3} from "./components/level3";
+import {Level4} from "./components/level4";
+import {Level5} from "./components/level5";
 import {useState} from "react";
 
 function App() {
 
-  const [levelSelection, setLevelSelection] = useState([level1, level2, level3, level4, level5]);
-  
+  let [levelSelection, setLevelSelection] = useState([Level1, Level2, Level3, Level4, Level5]);
+
+  let currentLevel = 0;
+
+  const levelSelect = () => {
+    currentLevel = Math.floor(Math.random() * 5 + 1);
+    setLevelSelection = currentLevel;
+  }
+
   return (
     <div className="App">
-      
+      <img></img>
+      <div className = "startGame">
+        <button onClick = {levelSelect}>Start Game</button>
+        <Level1 levelSelection = {levelSelection} />
+        <Level2 levelSelection = {levelSelection} />
+        <Level3 levelSelection = {levelSelection} />
+        <Level4 levelSelection = {levelSelection} />
+        <Level5 levelSelection = {levelSelection} />
+      </div>
     </div>
   );
 }
