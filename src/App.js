8232243b80f12b8971cc2d3779340c4a17401d3f
Chris Benson <ChrisBenson87@gmail.com>
2022-07-17 01:11:25 +0100

    testing stuff

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import {Level2} from "./components/level2";
 import {Level3} from "./components/level3";
 import {Level4} from "./components/level4";
 import {Level5} from "./components/level5";
+import {useState} from "react";
 
 function App() {
 
+  const [levelSelected, setLevelSelected] = useState(false);
   let currentLevel = 0;
 
   const levelSelect = () => {
@@ -16,16 +18,12 @@ function App() {
 
   if(currentLevel = 1) {
     return <Level1 />}
-
   else if(currentLevel = 2) {
     return <Level2 />}
-  
   else if(currentLevel = 3) {
     return <Level3 />}
-  
   else if(currentLevel = 4) {
     return <Level4 />}
-  
   else if(currentLevel = 5) {
     return <Level5 />}
 
